bf2d949bcbe565cb289af9a3b34d4f7dd60722d5
Ajay Yadav <121283256+ajayyadav3872@users.noreply.github.com>
2023-04-04 19:29:50 +0530

    Using State in Person.js

--- Person.js
+++ Person.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
-import Name from './Name';
+// import Demo from './Demo';
 class Person extends Component {
     constructor(props){
         super(props);
         this.props =props;
+        this.state = {
+            name: "Ajay Yadav",
+            age: 23,
+        };
+    }
+
+    nameChangeHandler(){
+        this.setState({name: "John"},() => {
+            console.log(this.state);
+        });
     }
     render() {
         return (
             <div>
-                <div>Name: {this.props.name}</div>
-                <div>Age: {this.props.Age} </div>
-                <Name/>
+                <h1>
+                    <i>
+                    Dear {this.state.name},
+                    {this.state.age} years,
+                     Welcome To React Complete Course.
+                    </i>
+                    <br/>
+                    <button onClick={() => this.nameChangeHandler()}>Click Me</button>
+                </h1>
             </div>
         );
     }
