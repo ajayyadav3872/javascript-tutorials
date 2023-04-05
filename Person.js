29d269cee4978d220178852917c78b1ae599f862
Ajay Yadav <121283256+ajayyadav3872@users.noreply.github.com>
2023-04-05 15:32:11 +0530

    add inline binding

--- Person.js
+++ Person.js
@@ -1,31 +1,18 @@
 import React, { Component } from 'react';
-// import Demo from './Demo';
+
 class Person extends Component {
     constructor(props){
         super(props);
         this.props =props;
-        this.state = {
-            name: "Ajay Yadav",
-            age: 23,
-        };
     }
 
-    nameChangeHandler(){
-        this.setState({name: "John"},() => {
-            console.log(this.state);
-        });
-    }
     render() {
         return (
             <div>
                 <h1>
-                    <i>
-                    Dear {this.state.name},
-                    {this.state.age} years,
-                     Welcome To React Complete Course.
-                    </i>
+                    <label>Name </label> : <i>{this.props.name}</i>
                     <br/>
-                    <button onClick={() => this.nameChangeHandler()}>Click Me</button>
+                    <label>Age </label> : <i>{this.props.age}</i>
                 </h1>
             </div>
         );
